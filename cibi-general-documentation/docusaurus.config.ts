a16f5373619fef4b04461915ae1f954f00369bc7
RusselG21 <contract.fullstackdev@cibi.com.ph>
2025-02-13 22:51:40 +0800

    Update docusaurus.config.ts

--- cibi-general-documentation/docusaurus.config.ts
+++ cibi-general-documentation/docusaurus.config.ts
@@ -10,10 +10,10 @@ const config: Config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://your-docusaurus-site.example.com",
+  url: "https://RusselG21.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/CIBI_General_Documentation/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
